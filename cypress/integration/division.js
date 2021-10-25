ed6b8e77db06a4be8d5b1797e557663c61fe49ea
Mateusz <mateusz@qunabu.com>
2021-10-25 16:10:32 +0200

    remove circile ci add github actions

--- cypress/integration/division.js
+++ cypress/integration/division.js
@@ -13,4 +13,18 @@ describe("Dummy divider", () => {
     cy.get('[data-testid="result"]').should("have.text", "1.5");
     cy.get('[data-testid="modulo"]').should("have.text", "1");
   });
+  it("Basic functionality 3/0 should be 🤬", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Dummy divider");
+    cy.get('input[data-testid="dzielna"]')
+      .clear()
+      .type("3")
+      .should("have.value", "3");
+    cy.get('input[data-testid="dzielnik"]')
+      .clear()
+      .type("0")
+      .should("have.value", "0");
+    cy.get('[data-testid="result"]').should("have.text", "🤬");
+    cy.get('[data-testid="modulo"]').should("have.text", "🤬");
+  });
 });
