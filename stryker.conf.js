1ae56e58e041a8d6da244138ddea0fe50d71f2ea
Mateusz <mateusz@qunabu.com>
2021-10-27 13:03:42 +0200

    styker mutation

---
+++ stryker.conf.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  _comment:
+    "This config was generated using 'stryker init'. Please see the guide for more information: https://stryker-mutator.io/docs/stryker-js/guides/react",
+  testRunner: "jest",
+  reporters: ["progress", "clear-text", "html", "dashboard"],
+  coverageAnalysis: "perTest",
+  jest: {
+    projectType: "create-react-app",
+  },
+  dashboard: {
+    project: "github.com/qunabu/js-testing-types",
+    //version: "main",
+    //module: "my-module",
+    baseUrl: "https://dashboard.stryker-mutator.io/api/reports",
+    reportType: "full",
+  },
+};
